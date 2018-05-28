83d0c07101f03b0ae319883dd59508559d4ed6bc
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2018-05-28 13:39:32 +0200

    :boom: Add room creation system, detect if room is full and malus informations

--- app.js
+++ app.js
@@ -26,8 +26,41 @@ app.get('/:room', (req, res) => {
 
 server.listen(2000)
 
-const io = require('socket.io')(server,{
+const io = require('socket.io')(server,{})
 
+// Interactions between sockets and server
+io.sockets.on('connection', (socket) => {
+
+    // Once a client has connected, he join a room
+
+    // Check how many people are in a room
+    const clientsInRoom = io.nsps['/'].adapter.rooms[room] // Array of all clients in the room
+    const numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length // Count number of clients
+    switch (numClients) {
+        case 0: // first client -> create the room
+            socket.join(room) // Client join the room
+            socket.emit('created', room, name)
+            break
+        case 1: // Seco²nd client -> join the room
+            socket.join(room) // Client join the room
+            socket.in(room).broadcast.emit('join', name) // Send message to the client already in the room
+            socket.emit('joined', room, name) // Send message to the client who joined
+            io.sockets.in(room).emit('ready') // Send a ready message to all clients of the room
+            break
+        case 2: // Already 2 clients in the room -> the room is full
+            socket.emit('err', 'The room ' + room + ' is full') // Send an error to the client
+            break
+    }
+    // Emit the malus to the other player in the same romm
+    socket.on('malus', (malus) => {
+        socket.in(room).broadcast.emit('malus', malus)
+        socket.in(room).emit('message', 'You got a malus !')
+    })
+
+    // Lose socket connection
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+    })
 })
 
 console.log('server is up')
