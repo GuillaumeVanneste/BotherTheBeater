cb77790feae1886e87b4a52246babe4272bd97c4
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2018-05-30 22:58:08 +0200

    Transform client.js into clientGame.js, script use only for the game

--- client/assets/scripts/clientGame.js
+++ client/assets/scripts/clientGame.js
@@ -1,21 +1,8 @@
 // set-up a connection between the client and the server
 const socket = io({transports: ['websocket'], upgrade: false})
 let isReady = false
-const roomsTable = document.querySelector("roomsTable")
-let allRooms = []
 
-socket.on("browser", (currentRooms) => {
-    for(let i = 0; i < currentRooms.length - 1; i++) {
-        const row = roomsTable.insertRow(i)
-        const cell1 = row.insertCell(0)
-        const cell2 = row.insertCell(1)
-        cell1.innerHTML = currentRooms[i]
-        cell2.innerHTML = "1/2"
-        console.log("create rooms")
-    }
-    allRooms = currentRooms
-    console.log("browser")
-})
+socket.emit('room')
 
 /**
  * Malus
@@ -63,12 +50,6 @@ socket.on('malus', (malus) => {
             break
     }
 })
-
-/**
- * Rooms brower
- */
-
-
 
 /**
  * Messages
