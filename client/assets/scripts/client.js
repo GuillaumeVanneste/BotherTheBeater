ec6daed132344f28c4d2c59a2a4c0353cd60d573
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2018-05-31 21:13:22 +0200

    :sparkles: Add a browser of current rooms and can connect to these rooms from modal

--- client/assets/scripts/client.js
+++ client/assets/scripts/client.js
@@ -1,16 +1,26 @@
 // set-up a connection between the client and the server
 const socket = io({transports: ['websocket'], upgrade: false})
-const roomsTable = document.querySelector('.roomsTable')
-let allRooms = []
+const $roomsTable = document.querySelector('.roomsTable')
+const $modalRoom = document.querySelector('.modalRoom')
 socket.on("browser", (currentRooms) => {
     for(let i = 0; i < currentRooms.length; i++) {
-        const row = roomsTable.insertRow(i)
+        const row = $roomsTable.insertRow(i)
         const cell1 = row.insertCell(0)
         const cell2 = row.insertCell(1)
+        const cell3 = row.insertCell(2)
         cell1.innerHTML = currentRooms[i]
         cell2.innerHTML = '1/2'
+        cell3.innerHTML = '<button type="sumbit" class="waves-effect waves-light btn-small orange modal-trigger" data-target="modal1">Join</button>'
         console.log("create rooms")
+        const triggerModal = cell3.querySelector('button')
+        triggerModal.addEventListener('mousedown', () => {
+            $modalRoom.value = currentRooms[i]
+        })
+
     }
-    allRooms = currentRooms
     console.log("browser")
+})
+
+$(document).ready(function(){
+    $('.modal').modal()
 })
